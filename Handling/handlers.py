e5017e26e42f8b7973e788588b035e3a20f00fba
Lorenzo <tunedconsulting@gmail.com>
2014-06-03 11:58:02 +0200

    Clean Old Dirs

--- Handling/handlers.py
+++
@@ -1,7 +0,0 @@
-
-import tornado.web
-
-class IndexHandler(tornado.web.RequestHandler):
-    def get(self):
-        greetings = 'Hello'
-        return self.render('index.html', greetings=greetings)
